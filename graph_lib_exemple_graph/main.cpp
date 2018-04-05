cf4a999d3874256f980b4d5298f177d03da962c8
karimelnahas <38103747+karimelnahas@users.noreply.github.com>
2018-04-05 16:14:24 +0200

    Add files via upload

--- graph_lib_exemple_graph/main.cpp
+++ graph_lib_exemple_graph/main.cpp
@@ -1,37 +1,10 @@
-#include "grman/grman.h"
 #include <iostream>
-
-#include "graph.h"
+#include "menu.h"
+using namespace std;
 
 int main()
 {
-    std::string NomDuGraph ="graph1";
-    /// A appeler en 1er avant d'instancier des objets graphiques etc...
-    grman::init();
-
-    /// Le nom du répertoire où se trouvent les images à charger
-    grman::set_pictures_path("pics");
-
-    /// Un exemple de graphe
-    Graph g;
-    g.Charger(NomDuGraph);
-
-
-    /// Vous gardez la main sur la "boucle de jeu"
-    /// ( contrairement à des frameworks plus avancés )
-    while ( !key[KEY_ESC] )
-    {
-        /// Il faut appeler les méthodes d'update des objets qui comportent des widgets
-        g.update(NomDuGraph);
-
-        /// Mise à jour générale (clavier/souris/buffer etc...)
-        grman::mettre_a_jour();
-    }
-    g.detruire_graph();
-    grman::fermer_allegro();
-
+  menu a;
+  a.choixgraph();
     return 0;
 }
-END_OF_MAIN();
-
-
